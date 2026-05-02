50dd06d7f4c8a2e323b342faeeebe15e39b24f9a
awakdeni <ikoawakdeni@gmail.com>
2026-05-03 03:20:54 +0700

    style: membatasi tampilan terminal agar hanya menampilkan 3 baris kode

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -92,7 +92,7 @@ export const CodeDisplay: React.FC<CodeDisplayProps> = ({
 
       <div 
         ref={containerRef}
-        className="relative font-mono text-2xl leading-[1.6] bg-[#0d0d0d] p-16 rounded-b-2xl border-x border-b border-white/5 shadow-2xl overflow-y-auto max-h-[480px] scrollbar-hide"
+        className="relative font-mono text-2xl leading-[1.6] bg-[#0d0d0d] p-10 rounded-b-2xl border-x border-b border-white/5 shadow-2xl overflow-y-hidden h-[190px] scrollbar-hide"
       >
         <div className="whitespace-pre-wrap break-words">
           {code.split('').map((char, index) => {
